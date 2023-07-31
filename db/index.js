af4d0b38bd651aab125ddbdcbe8404ad416576eb
Brian Gorman <briangorman99@gmail.com>
2023-07-31 02:53:18 -0400

    add jsdoc definitions to database functions, abstract read function

--- db/index.js
+++ db/index.js
@@ -1,32 +1,67 @@
-import { readFile, writeFile } from "fs/promises";
+import { writeFile } from "fs/promises";
 import { join } from "path";
-import { __dirname, removeElementFromArray } from "../utils/index.js";
+import { __dirname, removeElementFromArray, read } from "../utils/index.js";
 
+/** Path of the database file */
 const dbPath = join(__dirname, "db/db.json");
 
-const readDatabase = async () => readJSON(dbPath);
+/**
+ * @typedef {Object} item Database item
+ * @property {string} title Title of the item
+ * @property {string} text Text of the item
+ * @property {string} id Unique identifier
+ */
 
-const writeDatabase = async (data) => writeFile(dbPath, data);
+/** @typedef {Array<item>} items Array of {@link item} objects */
 
-const modifyDatabase = async (modify) => {
-  return readDatabase().then((data) => {
-    return writeDatabase(modify(data));
+/**
+ * Reads JSON from database file
+ * @returns {Promise<items>} Handle result of {@link read}
+ */
+const readDatabase = async () => read(dbPath);
+
+/**
+ * Writes data to the database file
+ * @param {items} data Items array
+ * @returns {Promise<void>} Handle result of {@link writeFile}
+ */
+const writeDatabase = async (data) => writeFile(dbPath, JSON.stringify(data));
+
+/**
+ * @callback modify Modifies database array
+ * @param {items} data Items array
+ * @returns {items} Modified array
+ */
+
+/**
+ * Reads the database file, modifies the array, then writes the modified array to the file.
+ * @param {modify} modify Callback that returns the modified database array
+ * @returns {Promise<void>} Handle result of {@link readFile} and {@link writeFile}
+ */
+const modifyDatabase = async (modify) =>
+  readDatabase().then((data) => writeDatabase(modify(data)));
+
+/**
+ * Adds an item to the database
+ * @param {item} item Item to be added
+ * @returns {Promise<void>} Handle result of {@link modifyDatabase}
+ */
+const addItemToDatabase = async (item) =>
+  modifyDatabase((data) => {
+    let items = data;
+    data.push(item);
+    return items;
   });
-};
 
-const addNoteToDatabase = async (note) => {
-  return modifyDatabase((data) => {
-    let notes = data;
-    data.push(note);
-    return notes;
+/**
+ * Removes an item from the database
+ * @param {string} id Unique `id` of the item to be removed
+ * @returns {Promise<void>} Handle result of {@link modifyDatabase}
+ */
+const removeItemFromDatabase = async (id) =>
+  modifyDatabase((data) => {
+    const items = removeElementFromArray(data, "id", id);
+    return items;
   });
-};
 
-const removeNoteFromDatabase = async (id) => {
-  return modifyDatabase((data) => {
-    const notes = removeElementFromArray(data, "id", id);
-    return notes;
-  });
-};
-
-export { readDatabase, addNoteToDatabase, removeNoteFromDatabase };
+export { readDatabase, addItemToDatabase, removeItemFromDatabase };
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,8 @@
 import { fileURLToPath } from "url";
-import { dirname, join } from "path";
+import { dirname, join, extname } from "path";
+import { readFile } from "fs/promises";
 
-/**
- * Root directory of the project
- */
+/** Root directory of the project */
 const __dirname = join(dirname(fileURLToPath(import.meta.url)), "../");
 
 /**
@@ -20,22 +19,28 @@ const removeElementFromArray = (array, property, value) => {
 };
 
 /**
- * Reads the JSON file, validates the content and integrity of the file,
- *  resolves with JSON or rejects with an error.
+ * Reads the file, validates the content and integrity of the file,
+ *  resolves with data or rejects with an error.
  *
  * Error conditions:
- * - JSON file is empty
- * - JSON could not be parsed
+ * - File is empty
+ * - Data could not be parsed
  *
- * @param path Path of the JSON file
- * @returns {Promise} Promise that resolves with the file contents as JSON
+ * @param path Path of the file
+ * @returns {Promise} Handle result of {@link readFile}
  */
-const readJSON = async (path) =>
+const read = async (path) =>
   readFile(path).then((data) => {
     if (data.length <= 0) throw new Error("File is empty");
-    let json = JSON.parse(data);
-    if (json instanceof SyntaxError) throw new Error("Could not parse JSON");
-    return json;
+    const ext = extname(path);
+    if (ext === "") throw new Error("Invalid file extension");
+    switch (ext) {
+      case ".json":
+        let json = JSON.parse(data);
+        if (json instanceof SyntaxError)
+          throw new Error("Could not parse JSON");
+        return json;
+    }
   });
 
-export { __dirname, removeElementFromArray, readJSON };
+export { __dirname, removeElementFromArray, read };
